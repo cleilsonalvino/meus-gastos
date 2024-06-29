a278c7500d97411833619dedd46e9b6945ab3ad2
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2024-06-29 00:40:01 -0300

    up

---
+++ public/src/script.js
@@ -0,0 +1,71 @@
+document.getElementById('openSheetsButton').addEventListener('click', function () {
+            // ID da planilha do Google Planilhas fornecida
+            var spreadsheetId = '1OAd1O-wpUJYcK03Wplq2cYyVtuOSFYwHH9sgSBuHmNU';
+            
+            var webUrl = 'https://docs.google.com/spreadsheets/d/' + spreadsheetId + '/edit';
+            
+    
+                window.location.href = webUrl;
+        });
+
+
+    async function Enviar() {
+      const item = document.getElementById('item').value;
+      const valor = document.getElementById('valor').value;
+      const data = document.getElementById('data').value;
+
+      setTimeout(()=>{
+      loader.style.opacity = '0'
+      loader.style.visibility = 'hidden'  
+      
+      }, 2500)
+
+      const loader = document.getElementById('loader');
+      loader.style.opacity = '1'
+      loader.style.visibility = 'visible'   
+
+
+      if (!item || !valor || !data) {
+        alert('Por favor, preencha todos os campos.');
+        return;
+      }
+
+      const newData = {
+        data: [
+          {
+            Item: item,
+            Valor: valor,
+            Data: data
+          }
+        ]
+      };
+
+      try {
+        const response = await fetch(sheetdbUrl, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(newData)
+        });
+
+        if (response.ok) {
+          setTimeout(()=>{
+            okk.style.opacity = '0'
+            okk.style.visibility = 'hidden'  
+            
+            }, 3000)
+            okk.style.opacity = '1'
+            okk.style.visibility = 'visible'  
+          // Limpar os campos após o envio
+          document.getElementById('item').value = '';
+          document.getElementById('valor').value = '';
+          document.getElementById('data').value = '';
+        } else {
+          alert('Erro ao enviar dados.');
+        }
+      } catch (error) {
+        console.error('Erro:', error);
+        alert('Erro ao enviar dados.');
+      }
+    }
