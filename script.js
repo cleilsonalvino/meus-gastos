b933e4bff2a3a49154502c9a61c8b59403b308fb
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2024-06-29 01:47:40 -0300

    up

--- script.js
+++ script.js
@@ -71,23 +71,4 @@ document.getElementById('openSheetsButton').addEventListener('click', function (
     }
 
 
-    const toggleThemeButton = document.getElementById('toggle-theme');
-
-    toggleThemeButton.addEventListener('click', () => {
-      const currentTheme = document.documentElement.getAttribute('data-theme');
-      const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-      document.documentElement.setAttribute('data-theme', newTheme);
-    });
-
-    // Opcional: Salvar a preferência de tema no localStorage
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme) {
-      document.documentElement.setAttribute('data-theme', savedTheme);
-    }
-
-    toggleThemeButton.addEventListener('click', () => {
-      const currentTheme = document.documentElement.getAttribute('data-theme');
-      const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-      document.documentElement.setAttribute('data-theme', newTheme);
-      localStorage.setItem('theme', newTheme);
-    });
+    
